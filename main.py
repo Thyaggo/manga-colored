27f416a3a56cd1b34b430f1e3479303e4afffca5
Thyaggo <sgarciabedoya@gmail.com>
2024-10-30 20:06:21 +0000

    Ready for Training

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ from skimage import color
 import lightning as L
 from lightning.pytorch.loggers import WandbLogger
 import torch
-import torchvision.transforms as T
 from diffusers.models.unets.unet_2d import UNet2DModel
 from torch.utils.data import DataLoader, random_split
+import torch.nn.functional as F
 from datasets import load_dataset
 
 # Data and training parameters
 num_workers: int = 4
 max_epochs: int = 50
-lr = 1e-3
+lr = 1e-4
 save_images = True
-wandb = False
+wandb = True
 
 torch.set_float32_matmul_precision("high")
 
 @dataclass
 class Config():
     # Model
-    sample_size: int = 1024
+    sample_size: int = 512
     in_channels: int = 1
     out_channels: int = 2
     timestep: int = 1
-    layers_per_block=1  # how many ResNet layers to use per UNet block
+    layers_per_block= 2  # how many ResNet layers to use per UNet block
     block_out_channels=(128, 256, 512)  # the number of output channels for each UNet block
     down_block_types=(
         "DownBlock2D",
@@ -43,7 +43,7 @@ class Config():
     # Data
     dataset: str = "MichaelP84/manga-colorization-dataset"
     train_val_split: float = 0.99
-    train_batch_size: int = 1
+    train_batch_size: int = 4
     streaming = False
     
 
@@ -71,11 +71,14 @@ class MangaColorizationDataModule(L.LightningDataModule):
         color_images = torch.stack([item["color_image"] for item in batch])
         bw_images = torch.stack([item["bw_image"] for item in batch])
         
-        # Convertir las imágenes de RGB a LAB y ajustar las dimensiones
-        y = color.rgb2lab(color_images.permute(0, 2, 3, 1).numpy())
-        y = torch.tensor(y, dtype=torch.float).permute(0, 3, 1, 2)[:, 1:]  # Excluir canal L
+        color_images = F.interpolate(color_images, size=(self.config.sample_size, self.config.sample_size), mode="bilinear", align_corners=False)
+        bw_images = F.interpolate(bw_images, size=(self.config.sample_size, self.config.sample_size), mode="bilinear", align_corners=False)
         
-        return bw_images.to(dtype=y.dtype), y
+        # Convertir las imágenes de RGB a LAB y ajustar las dimensiones
+        color_images = color.rgb2lab(color_images.permute(0, 2, 3, 1).numpy())
+        color_images = torch.tensor(color_images, dtype=torch.float).permute(0, 3, 1, 2)[:, 1:]  # Excluir canal L
+        
+        return bw_images.to(dtype=color_images.dtype), color_images
     
     
 class MangaColorizationModel(L.LightningModule):
@@ -93,21 +96,22 @@ class MangaColorizationModel(L.LightningModule):
             up_block_types=config.up_block_types
         )
 
-    def forward(self, x):
-        return self.unet(x)
-
     def training_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self.unet(x, config.timestep).sample
+        
+        assert x.size(-1) == config.sample_size, f"Expected sample size {config.sample_size}, got {x.size(-1)}"
+        
+        with torch.autocast(device_type='cuda' if torch.cuda.is_available() else 'cpu' ,dtype=torch.bfloat16):
+            y_hat = self.unet(x, config.timestep).sample
+            
         loss = self.loss(y_hat, y)
         self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self.unet(x, config.timestep).sample
-        if wandb:
-            logger.log_image("color", torch.cat([x, y_hat], dim=1))
+        with torch.autocast(device_type='cuda' if torch.cuda.is_available() else 'cpu' ,dtype=torch.bfloat16):
+            y_hat = self.unet(x, config.timestep).sample
         
         # Save the image locally
         if save_images:
@@ -118,7 +122,9 @@ class MangaColorizationModel(L.LightningModule):
                 os.makedirs("output_images")
             for i, img in enumerate(color_image):
                 img_path = f"output_images/validation_image_{batch_idx}_{i}.png"
-                plt.imsave(img_path, img)
+                plt.imsave(img_path, img.squeeze())
+                if wandb:
+                    logger.log_image("color", [img.squeeze()])
         
         loss = self.loss(y_hat, y)
         self.log("val_loss", loss)
@@ -135,11 +141,9 @@ if __name__ == "__main__":
                          name = "test_dev") if wandb else None
     dm = MangaColorizationDataModule(config)
     model = MangaColorizationModel(config)
-    trainer = L.Trainer(fast_dev_run = True,
+    trainer = L.Trainer(fast_dev_run = False,
                         logger=logger,
                         accelerator="auto",
                         max_epochs=max_epochs
                         )
     trainer.fit(model, dm)
-    
-
